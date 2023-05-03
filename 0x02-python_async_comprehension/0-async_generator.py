f77efd683b24714052719cc7b253630b1a04c616
keem2000 <joycelinaomari@gmail.com>
2023-05-03 14:56:15 +0000

    measure

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
+"""Write a coroutine called async_generator that takes no arguments.
+The coroutine will loop 10 times, each time asynchronously wait 1 second,
+then yield a random number between 0 and 10. Use the random module.
+"""
 
-""" Async Comprehensions """
 
-from asyncio import sleep
-from random import uniform
-from typing import AsyncGenerator, Generator
+import asyncio
+import random
+from typing import Generator
+
 
 async def async_generator() -> Generator[float, None, None]:
-    """ Async Generator """
-    for _ in range(10):
-        await sleep(1)
-        yield uniform(0, 10)
+    """Loop 10 times, wait 1 sec each time"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,17 +1,22 @@
-#ython3
+#!/usr/bin/env python3
+"""Import async_comprehension from the previous file and
+write a measure_runtime coroutine that will execute async_comprehension
+four times in parallel using asyncio.gather.
+measure_runtime should measure the total runtime and return it.
+Notice that the total runtime is roughly 10 seconds, explain it to yourself.
+"""
 
-""" Async Comprehensions """
 
-from asyncio import gather
-from time import time
-
+import time
+import asyncio
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """ Run time for four parallel comprehensions """
-    start = time()
-    tasks = [async_comprehension() for i in range(4)]
-    await gather(*tasks)
-    end = time()
-    return (end - start)
+    """Measure the total runtime"""
+    start_time = time.time()
+
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
+
+    end_time = time.time()
+    return end_time - start_time
